ea2e6de33976f94ec63b1533dac6764915f0ea05
Ashish Langhe <langheashish389@gmail.com>
2020-06-09 17:00:46 +0530

    Chapter - CH04 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
-import 'package:dev01/change_name_card.dart';
+
 import 'package:dev01/my_drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,11 +13,22 @@ class _HomePageState extends State<HomePage> {
 
   TextEditingController _nameController = TextEditingController();
   var myText = "Change Me";
+  var url = "http://jsonplaceholder.typicode.com/photos";
+  var data;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+
+    getData();
+  }
+
+  getData() async {
+     var res = await http.get(url);
+     data = jsonDecode(res.body);
+     setState(() {});
+     print(data);
   }
 
   @override
@@ -27,10 +40,24 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Card(
-            child: ChangeNameCard(myText: myText, nameController: _nameController),
-          ),
+        child: data != null ? ListView.builder(
+          itemBuilder: (context,index){
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ListTile(
+                leading: Image.network(data[index]["url"]),
+                title: Text(data[index]["title"],
+                style: TextStyle(
+                  fontWeight: FontWeight.bold
+                ),
+                ),
+               subtitle: Text("ID : ${data[index]["id"]}"),
+              ),
+            );
+          },
+          itemCount: data.length,
+        ) : Center(
+          child: CircularProgressIndicator(),
         ),
       ),
       drawer: MyDrawer(),
